00ea295a9d76a875105305ebca950598218bcff6
Diwakar <diwakarn1599@gmail.com>
2021-07-03 13:29:01 +0530

    [Diwakar] Usecase 1 - created addressbook and add contacts

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,26 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Program");
+            
+            while (true)
+            {
+                Console.WriteLine("*********************************************************");
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        AddressBookDetails.AddMember();
+                        break;
+                    case 2:
+                        AddressBookDetails.ViewContacts();
+                        break;
+                    case 3:
+                        // to exit from main method
+                        return;
+
+                }
+            }
         }
     }
 }
